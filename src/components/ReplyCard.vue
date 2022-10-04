2330a947628f58778642fca64ac400a296afb565
imabby0508 <imabby0508@gmail.com>
2022-10-05 00:12:09 +0800

    rebase from main

---
+++ src/components/ReplyCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="reply__card__wrapper">
+    <div class="reply__card">
+      <img class="reply__card__avatar" src="@/assets/image/user-avator.png" alt="avatar">
+      <div class="reply__card__right">
+        <div class="reply__card__title">
+          <p class="reply__card__name">Devon Lane</p>
+          <p class="reply__card__account">@devon-lane</p>
+          <p class="reply__card__time">・12小時</p>
+        </div>
+        <div class="reply__card__target">
+          <p>回覆</p>
+          <p class="reply__card__target__account">@apple</p>
+        </div>
+        <p class="reply__card__content">
+          XXXXXXXXXXXXXXXXXXXXXXX
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
