84b3f029dba180ada072ad36e126f72723a996e9
imabby0508 <imabby0508@gmail.com>
2022-10-04 19:44:06 +0800

    add main page

---
+++ src/components/TweetCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tweet__card">
+    <div class="tweet__card__content">
+      <img class="tweet__card__avatar" src="@/assets/image/user-avatar.png" alt="avatar">
+      <div class="tweet__card__right">
+        <div class="tweet__card__title">
+          <p class="tweet__card__name">Apple</p>
+          <p class="tweet__card__account">@apple</p>
+          <p class="tweet__card__time">●3小時</p>
+        </div>
+        <p class="tweet__card__content">
+          XXXXXXXXXXXXXXXXXXXXXXX
+        </p>
+        <div class="tweet__card__footer" >
+          <img src="@/assets/image/reply-icon.png" alt="reply">
+          <p>13</p>
+          <img src="@/assets/image/like-icon.png" alt="like">
+          <p>76</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
