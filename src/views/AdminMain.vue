2fd6a3896a068040722c7a05e9c5f47b02c9e123
Bun-letsgogo <bun.coding@gmail.com>
2022-10-09 11:52:03 +0800

    setup admin pages

--- src/views/AdminMain.vue
+++ src/views/AdminMain.vue
@@ -7,7 +7,38 @@
         <h4>推文清單</h4>
       </div>
       <div class="main__post">
-        <div class=""></div>
+
+
+        <div class="tweet__card__wrapper">
+          <div
+            v-for="tweet in tweetsArray"
+            :key="tweet.id"
+            class="tweet__card"
+            :to="{name: 'reply', params: {id: tweet.id}}"
+          >
+            <div class="tweet__card__content">
+              <div class="tweet__card__title">
+                <img
+                  class="tweet__card__avatar"
+                  src="@/assets/image/user-avatar.png"
+                  alt="avatar"/>
+                <p class="tweet__card__name">{{ tweet.user.name }}</p>
+                <p class="tweet__card__account">@{{ tweet.user.account }}</p>
+                <p class="tweet__card__time">・{{ tweet.createdAt }}</p>
+              </div>
+              <div class="tweet__card__description">
+                {{ tweet.description }}
+              </div>
+            </div>
+            <img 
+            @click.stop.prevent="deleteTweet(tweet.id)"
+            src="@/assets/image/black-cross.png" 
+            class="tweet__card__delete" 
+            alt="delete-tweet-icon">
+          </div>
+        </div>
+
+
       </div>
     </div>
   </div>
@@ -16,10 +47,196 @@
 <script>
 import AdminNav from '@/components/AdminNav.vue';
 
+const dummyData = {
+  tweets: [
+    {
+      id: 1,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: false,
+      user: {
+        id: 1,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 2,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 3,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 4,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 5,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 6,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 7,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+    {
+      id: 8,
+      description:
+        "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
+      createdAt: "3小時",
+      replyCounts: 13,
+      likeCounts: 76,
+      isLiked: true,
+      user: {
+        id: 2,
+        account: "apple",
+        name: "Apple",
+        avatar: "",
+      },
+    },
+  ],
+};
+
 export default {
   components: {
     AdminNav,
-  }
+  },
+  data() {
+    return {
+      tweet: {
+        id: -1,
+        description: "",
+        createdAt: "",
+        replyCounts: "",
+        likeCounts: "",
+        isLiked: false,
+        user: {
+          id: -1,
+          account: "",
+          name: "",
+          avatar: "",
+        },
+      },
+      tweetsArray: []
+    };
+  },
+  created() {
+    this.fetchTweets();
+  },
+  methods: {
+    fetchTweets() {
+      dummyData.tweets.forEach((tweet) => {
+        const {
+          id,
+          description,
+          createdAt,
+          replyCounts,
+          likeCounts,
+          isLiked,
+          user,
+        } = tweet;
+
+        const { id: userId, account, name, avatar } = user;
+
+        this.tweet = {
+          ...this.tweet,
+          id,
+          description,
+          createdAt,
+          replyCounts,
+          likeCounts,
+          isLiked,
+          user: {
+            id: userId,
+            account,
+            name,
+            avatar,
+          },
+          showReplyModal: false
+        };
+
+        this.tweetsArray.push(this.tweet)
+      });
+    },
+    deleteTweet (tweetId) {
+      this.tweetsArray = this.tweetsArray.filter(tweet => (tweet.id !== tweetId))
+    }
+  },
 }
 </script>
 
@@ -30,15 +247,15 @@ export default {
 }
 
 .main {
-  position: absolute; //讓 setting置於 nav, popularlist 中間
+  position: absolute; //讓 main置於中間
   left: 14.4vw;
-  right: 21.2vw;
+  right: 0;
 
   .main__title {
     position: fixed; //讓 title固定在上面
     top: 0;
     left: 23.7vw;
-    right: 30.5vw;
+    right: 9.3vw;
     z-index: 99;
     padding: 24px;
     margin-bottom: 0;
@@ -49,11 +266,61 @@ export default {
   .main__post {
     position: absolute; //讓 setting排在 title下面
     width: 100%;
-    height: 90vh;
+    height: 100vh;
     top: 80px;
-    padding: 24px 23px 0 23px;
+    bottom: 0;
     border-left: 1px solid $border;
     border-right: 1px solid $border;
+  }
+
+  .tweet__card {
+    position: relative;
+    .tweet__card__delete {
+      position: absolute;
+      top: 17px;
+      right: 0;
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+    }
+  }
+
+  .tweet__card__content {
+    padding: 16px 24px;
+    border-bottom: 1px solid $border;
+
+    .tweet__card__title {
+      display: flex;
+      align-items: top;
+      height: 40px;
+
+      .tweet__card__avatar {
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+      }
+
+      .tweet__card__name {
+        margin: 2px 8px 0 10px;
+        font-size: 16px;
+        font-weight: 700;
+      }
+
+      .tweet__card__account,
+      .tweet__card__time {
+        margin: 4px 0 0 0;
+        color: $secondary-gray;
+        font-size: 14px;
+        font-weight: 400;
+      }
+    }
+    .tweet__card__description {
+      color: $scale-gray10;
+      font-size: 16px;
+      font-weight: 400;
+      margin: 0;
+      padding: 0px 0 5px 60px;
+    }
   }
 }
 </style>
--- src/views/AdminTweetersCard.vue
+++ src/views/AdminTweetersCard.vue
@@ -1,12 +1,43 @@
 <template>
 
-  <div class="setting__container">
+  <div class="main__container">
     <AdminNav />
-    <div class="setting">
-      <div class="setting__title">
-        <h4>推文清單</h4>
+    <div class="main">
+      <div class="main__title">
+        <h4>使用者列表</h4>
       </div>
-      <div class="setting__post">
+      <div class="main__post">
+
+
+        <div class="tweet__card__wrapper">
+          <div class="card d-flex flex-column" v-for="user in users" :key="user.id">
+            <img class="card--cover" :src="user.backgroundImage" alt="user-cover">
+            <img class="card--avatar" :src="user.avatar" alt="user-avatar">
+
+            <div class="card--info text-center">
+              <p class="card--info--name">{{user.name}}</p>
+              <p class="card--info--account">@{{user.account}}</p>
+            </div>
+
+            <div class="card--tweet d-flex justify-content-between">
+              <div class="card--tweet--posts d-flex">
+                <img class="icon" src="@/assets/image/tweet-icon.png" alt="tweet-icon">
+                <p>{{user.tweetCount}}</p>
+              </div>
+              <div class="card--tweet--likes d-flex">
+                <img class="icon" src="@/assets/image/like-icon.png" alt="like-icon">
+                <p>{{user.likeCount}}</p>
+              </div>
+            </div>
+
+            <div class="card--follow d-flex justify-content-center">
+              <p>{{user.followerCount}}個 <span>跟隨中</span></p>
+              <p>{{user.followingCount}}個 <span>跟隨者</span></p>
+            </div>
+
+          </div>
+        </div>
+
 
       </div>
     </div>
@@ -16,29 +47,251 @@
 <script>
 import AdminNav from '@/components/AdminNav.vue';
 
+const dummyData = {
+  users: [
+    {
+      id: 1,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 2,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 3,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 4,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 5,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+    avatar: "https://picsum.photos/150",
+    backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 6,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+    avatar: "https://picsum.photos/150",
+    backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 7,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 8,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 9,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 10,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 11,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+    {
+      id: 12,
+      account: "root",
+      email: "root@example.com",
+      passwor: "$2a$10$0svlV32uVwK3XCsCbOnyG.Aphf8NcHVEA9ZNrrlCnKCyVF0GQEUGa",
+      name: "root",
+      avatar: "https://picsum.photos/150",
+      backgroundImage: "https://picsum.photos/300/150",
+      introduction: "Repellat ut voluptatem. Natus amet aliquam sint et quae ut fuga aut.",
+      role: "admin",
+      createdAt: "2022-10-04T10:14:28.000Z",
+      updatedAt: "2022-10-04T10:14:28.000Z",
+      tweetCount: 10,
+      likeCount: 3,
+      followerCount: 2,
+      followingCount: 2
+    },
+  ]
+}
+
 export default {
   components: {
     AdminNav,
-  }
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      this.users = dummyData.users
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.setting__container {
+.main__container {
   position: relative;
-  margin: auto 9.3vw auto 9.3vw;
+  margin: auto 0 auto 9.3vw;
 }
 
-.setting {
-  position: absolute; //讓 setting置於 nav, popularlist 中間
+.main {
+  position: absolute; //讓 main置於中間
   left: 14.4vw;
-  right: 21.2vw;
+  right: 0;
 
-  .setting__title {
+  .main__title {
     position: fixed; //讓 title固定在上面
     top: 0;
     left: 23.7vw;
-    right: 30.5vw;
+    right: 0;
     z-index: 99;
     padding: 24px;
     margin-bottom: 0;
@@ -46,14 +299,95 @@ export default {
     border: 1px solid $border;
   }
 
-  .setting__post {
+  .main__post {
     position: absolute; //讓 setting排在 title下面
     width: 100%;
-    height: 90vh;
+    height: 100vh;
     top: 80px;
-    padding: 24px 23px 0 23px;
+    bottom: 0;
+    right: 0;
+    padding: 16px 8px 0 8px;
     border-left: 1px solid $border;
     border-right: 1px solid $border;
   }
+
+  .tweet__card__wrapper {
+    // padding: 16px 8px 0px 15px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 249px);
+    width: 100%;
+    grid-gap: 16px;
+    justify-content: center;
+    .card {
+      position: relative;
+      background: #f6f7f8;
+      border-radius: 10px;
+      width: 249px;
+      height: 314px;
+      &--cover {
+        object-fit: cover;
+        width: 100%;
+        height: 140px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+      }
+      &--avatar {
+        object-fit: cover;
+        position: absolute;
+        top: 64px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border: 6px solid $scale-gray1;
+      }
+      &--info {
+        margin-top: 32px;
+        text-align: center;
+        &--name {
+          color: $modal-black;
+          font-weight: 700;
+          font-size: 16px;
+          line-height: 26px;
+        }
+        &--account {
+          color: $secondary-gray;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 22px;
+        }
+      }
+      &--tweet {
+        margin: 16px auto 8px auto;
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 26px;
+        &--posts {
+          margin-right: 16px;
+        }
+      }
+      &--follow {
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 22px;
+        p:first-child {
+          margin-right: 8px;
+        }
+        span {
+          color: $secondary-gray;
+        }
+      }
+    }
+    .icon {
+      width: 24px;
+      height: 24px;
+      margin-right: 8px;
+    }
+  }
+
+
 }
 </style>
